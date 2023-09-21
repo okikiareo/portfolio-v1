c233647b3ceff757022f56aa0852cf74812a98d9
Okiki Areo <okikiareo@gmail.com>
2023-09-21 21:20:58 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
-let swiper = new Swiper('.swiper-container', {
-    cssMode: true,
-    loop: true,
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-    },
-    mousewheel: true,
-    keyboard: true,
-});
+// let swiper = new Swiper('.swiper-container', {
+//     cssMode: true,
+//     loop: true,
+//     navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+// });
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
