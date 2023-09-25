a6232cd11c935caeb51f39251206044667fb265a
Okiki Areo <okikiareo@gmail.com>
2023-09-25 01:11:59 +0100

    done js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,52 @@ if(navClose) {
       navMenu.style.bottom = "-100%"
   })
 }
-
 }
 
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav_link")
+
+navLink.forEach((item) => {
+    item.addEventListener("click", () => {
+        navMenu.style.bottom = "-100%"
+    })
+})
 
 
+// HIDE MENU IF IT'S OPEN AND USER CLICK ON THE BACKGROUND
+const dropMenu = () => {
+    navMenu.style.bottom = "-100%";
+}
+
+if(main) {
+    main.addEventListener("click", dropMenu)
+}
+
+if(footer) {
+    footer.addEventListener("click", dropMenu)
+}
+/*===== SCROLL SECTIONS ACTIVE LINK =======*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*======= SHOW SCROLL UP =====*/ 
 
 // let slideIndex = 1;
 // showSlides(slideIndex);
@@ -106,3 +147,42 @@ let carousel = document.getElementById("move"); // Replace with your carousel's 
 carousel.addEventListener("mouseenter", pauseTimer);
 carousel.addEventListener("click", pauseTimer);
 carousel.addEventListener("mouseleave", restartTimer);
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*====== FOOTER =====*/
+const currentYear = document.querySelector("#current-year")
+
+const yearText = new Date().getFullYear();
+
+currentYear.textContent = yearText.toString()
+
